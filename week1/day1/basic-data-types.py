da0f246555721f947f72408742333453c1b47ffe
iambetelhem24 <betelhemlegesse16@gmail.com>
2025-11-09 15:57:33 +0200

    the first file

---
+++ week1/day1/basic-data-types.py
@@ -0,0 +1,52 @@
+'juliana'
+type('juliana')
+'JULIANA'.lower()
+my_name = 'Juliyana'
+my_name.upper()
+
+my_name = 'Juliana'
+name_upper = my_name.upper()
+
+print(name_upper)
+
+
+sentence = 'I love python\n'
+print(sentence * 3)
+
+sentence = 'I\'m in love with python\t'
+print(sentence * 3)
+
+sentence_js = sentence.replace('python', 'JavaScript')
+print(sentence_js)
+
+price = '15$'
+clean_price = int(price.replace('$', ''))
+print(clean_price)
+
+first_name = 'Ron'
+print(5==3)
+
+calculation = 5+6
+print(calculation)
+
+general_var = 'Hello'
+general_var = '456'
+general_var = 'True'
+print(general_var)
+x = 1
+y = 2
+x, y = y ,x
+print("x =", x)
+print("y =", y)
+
+temp = y
+y = x
+x = temp
+print(x,y)
+
+output = ''
+
+output += 'J'
+output += 'G'
+
+
