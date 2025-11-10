554cbeb83ad5c2e5a6f5c127f785cc73ff03be15
iambetelhem24 <betelhemlegesse16@gmail.com>
2025-11-10 20:38:05 +0200

    Add 10-char string validation and manipulation

--- week1/day1/daily-challenge.py
+++ week1/day1/daily-challenge.py
@@ -1,43 +1,22 @@
 ## Daily Challenge: Build up a string
+user_string = input("Enter a string that is exactly 10 characters long: ")
 
-### What You Will Learn:
-   # Python Basics
-   #  Conditionals
-   #Loops
-#1. **Ask for User Input:**
-   # The string **must be exactly 10 characters long**.
-
-#2. **Check the Length of the String:**
-   # If the string is **less than 10 characters**, print: `"String not long enough."`
-   # If the string is **more than 10 characters**, print: `"String too long."`
-   # If the string is **exactly 10 characters**, print: `"Perfect string"` 
-
-#3. **Print the First and Last Characters:**
-   #- Once the string is validated, print the **first** and **last** characters.
-#4. **Build the String Character by Character:**
-
-#5. **Bonus: Jumble the String (Optional)**
-
-#s = input("Enter a 10-character string: ")
-
-import random
-
-s = input("Enter a 10-character string: ")
-
-if len(s) < 10:
+if len(user_string) < 10:
     print("String not long enough.")
-elif len(s) > 10:
+elif len(user_string) > 10:
     print("String too long.")
 else:
-    print("Perfect string")
-    print("First character:", s[0])
-    print("Last character:", s[-1])
-    
-    # Build string character by character
-    for i in range(1, len(s)+1):
-        print(s[:i])
-    
-    # Bonus: Jumble the string
-    lst = list(s)
-    random.shuffle(lst)
-    print("Jumbled string:", ''.join(lst))
+    print("Perfect string!")
+
+
+    print("First character:", user_string[0])
+    print("Last character:", user_string[-1])
+
+    for i in range(1, len(user_string) + 1):
+        print(user_string[:i])
+
+    import random
+    char_list = list(user_string)
+    random.shuffle(char_list)
+    jumbled_string = ''.join(char_list)
+    print("Jumbled string:", jumbled_string)
