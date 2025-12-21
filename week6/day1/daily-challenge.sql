1ce1b3758933b8a0b5fd619d90b815ddf8829647
iambetelhem24 <betelhemlegesse16@gmail.com>
2025-12-21 22:32:32 +0200

    Add solutions for Daily Challenge: Actors - SQL queries

---
+++ week6/day1/daily-challenge.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE actors (
+--     actor_id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(100),
+--     last_name VARCHAR(100),
+--     date_of_birth DATE,
+--     nationality VARCHAR(50)
+-- );
+
+-- SELECT COUNT(*) FROM actors;
+
+-- INSERT INTO actors (first_name, last_name, date_of_birth, nationality) 
+-- VALUES ('John', '', NULL, 'American');
+
+
+
