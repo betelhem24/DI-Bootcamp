c51653e82328c3a4dfcc18c7729c7c562bad9bfb
iambetelhem24 <betelhemlegesse16@gmail.com>
2025-12-27 16:15:59 +0200

    Add predictions.txt, queries.sql, and setup.sql for week6/day2 SQL puzzle challenge

---
+++ week6/day2/daily-challenge-sql-puzzle/queries.sql
@@ -0,0 +1,19 @@
+-- -- Q1: What will be the OUTPUT?
+-- SELECT COUNT(*) 
+-- FROM FirstTab AS ft 
+-- WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL );
+
+-- -- Q2: What will be the OUTPUT?
+-- SELECT COUNT(*) 
+-- FROM FirstTab AS ft 
+-- WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 );
+
+-- -- Q3: What will be the OUTPUT?
+-- SELECT COUNT(*) 
+-- FROM FirstTab AS ft 
+-- WHERE ft.id NOT IN ( SELECT id FROM SecondTab );
+
+-- -- Q4: What will be the OUTPUT?
+-- SELECT COUNT(*) 
+-- FROM FirstTab AS ft 
+-- WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL );
---
+++ week6/day2/daily-challenge-sql-puzzle/setup.sql
@@ -0,0 +1,28 @@
+-- -- Create FirstTab table
+-- CREATE TABLE FirstTab (
+--      id integer, 
+--      name VARCHAR(10)
+-- );
+
+-- -- Insert data into FirstTab
+-- INSERT INTO FirstTab VALUES
+-- (5,'Pawan'),
+-- (6,'Sharlee'),
+-- (7,'Krish'),
+-- (NULL,'Avtaar');
+
+-- -- View FirstTab data
+-- SELECT * FROM FirstTab;
+
+-- -- Create SecondTab table
+-- CREATE TABLE SecondTab (
+--     id integer 
+-- );
+
+-- -- Insert data into SecondTab
+-- INSERT INTO SecondTab VALUES
+-- (5),
+-- (NULL);
+
+-- -- View SecondTab data
+-- SELECT * FROM SecondTab;
