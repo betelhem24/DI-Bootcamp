a41ffa85194a5075b68f9ab4770c0522e3591fb1
iambetelhem24 <betelhemlegesse16@gmail.com>
2025-12-30 18:02:10 +0200

    Add new files for exercises 1, 2, and 3 including .gitignore, app.js, package.json, and route files.

--- week7/day1/exercises-xp/exercise2/app.js
+++ week7/day1/exercises-xp/exercise2/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Middleware to parse JSON request bodies - THIS IS CRITICAL!
+// Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Import the todos router
@@ -14,5 +14,5 @@ app.use('/todos', todosRouter);
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:3000`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
